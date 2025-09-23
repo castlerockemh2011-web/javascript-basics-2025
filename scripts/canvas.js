2d31311b5a5d7eb11d4996f61442d097935b70fe
Robert Hardy <rhardy@dcsdk12.org>
2025-09-23 15:30:58 -0600

    Add canvas files

---
+++ scripts/canvas.js
@@ -0,0 +1,8 @@
+//@ts-check
+
+/** @type { HTMLCanvasElement } */
+//@ts-ignore This is an HTML Canvas
+const canvas = document.getElementById("game-canvas");
+
+const ctx = canvas.getContext("2d");
+

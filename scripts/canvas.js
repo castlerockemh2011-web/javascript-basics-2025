3226c0dbc0c230634b00f11db85c227fb4300a73
Robert Hardy <rhardy@dcsdk12.org>
2025-09-23 18:21:01 -0600

    Draw shapes on the canvas

--- scripts/canvas.js
+++ scripts/canvas.js
@@ -2,7 +2,24 @@
 
 /** @type { HTMLCanvasElement } */
 //@ts-ignore This is an HTML Canvas
-const canvas = document.getElementById("game-canvas");
+const CANVAS = document.getElementById("game-canvas");
 
-const ctx = canvas.getContext("2d");
+/** @type { CanvasRenderingContext2D } */
+//@ts-ignore is not null
+const CTX = CANVAS.getContext("2d");
 
+const HEIGHT = 600;
+const WIDTH = 800;
+
+CANVAS.height = HEIGHT;
+CANVAS.width = WIDTH;
+
+CTX.fillStyle = "black";
+CTX.fillRect(0, 0, WIDTH, HEIGHT);
+
+CTX.fillStyle = "red";
+CTX.fillRect(50, 0, WIDTH, HEIGHT);
+
+CTX.fillStyle = "purple";
+CTX.arc(WIDTH / 2, HEIGHT / 2, 100, 0, 2 * Math.PI);
+CTX.fill();
